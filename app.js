00b81fb7d5a60becd2be80b82d3d0bc80070c3a6
mtalalned <muhammadtalalned@gmail.com>
2024-08-02 11:13:17 -0700

    array assignment completed

--- app.js
+++ app.js
@@ -67,12 +67,92 @@
 // console.log ('Score of '+studentNames[2]+' is '+usamaTotal+'. Percentage: '+usamaTotal/totalMarks * 100 + '%')
 
 // Question No. 12
-var arr = ['red','orange','green','purple','brown']
-var beginningColor = prompt ('Enter color you want to add to the beginning')
-arr.unshift (beginningColor)
-console.log (arr)
-var endColor = prompt ('Enter color you want to add to the end')
-arr.push (endColor)
-console.log (arr)
-arr.unshift('mergenda','black')
-console.log (arr)
+// var arr = ['red','orange','green','purple','brown']
+// var beginningColor = prompt ('Enter color you want to add to the beginning')
+// arr.unshift (beginningColor)
+// console.log (arr)
+// var endColor = prompt ('Enter color you want to add to the end')
+// arr.push (endColor)
+// console.log (arr)
+// arr.unshift('mergenda','black')
+// console.log (arr)
+// arr.shift()
+// console.log (arr)
+// arr.pop()
+// console.log (arr)
+
+// var userInput = +prompt ('At which index you want to add the color ?')
+// var colorName = prompt ('Enter name of the color you want to add at the desired index')
+// arr.splice(userInput, 0, colorName)
+// console.log (arr)
+
+// var userInput = +prompt ('At which index you want to delete the color ?')
+// var noOfcolors = +prompt ('How many colors you want to delete ?')
+// arr.splice(userInput, noOfcolors)
+// console.log (arr)
+
+// Question No. 13
+// var engMarks = +prompt ('Enter English Marks')
+// var mathMarks = +prompt ('Enter Math Marks')
+// var urduMarks = +prompt ('Enter Urdu Marks')
+// var islMarks = +prompt ('Enter Islamiat Marks')
+
+// var arr = [engMarks , mathMarks , urduMarks , islMarks]
+// var sortedarr = arr.sort()
+// console.log (sortedarr)
+
+// Question No. 14
+// var fruits = ['strawberry' , 'apple' , 'orange' , 'banana']
+// var sortedFruits = fruits.sort ()
+// console.log (sortedFruits)
+
+// Question No. 15
+// var cities = ['Karachi' , 'Lahore' , 'Islamabad' , 'Quetta' , 'Peshawar']
+// var copyofArray = cities.slice (1 , 4)
+// console.log (copyofArray)
+
+// Question No. 16
+// var arr = ['This' , 'is' , 'my' , 'cat']
+// var arr1 = arr.join(' ')
+// console.log (arr1)
+
+// Question No. 17
+// var arr = []
+// arr.push ('keyboard')
+// arr.push ('mouse')
+// arr.push ('printer')
+// arr.push ('monitor')
+// console.log (arr)
+
+
+// Question No. 18
+// var arr = []
+// arr.unshift ('keyboard')
+// arr.unshift ('mouse')
+// arr.unshift ('printer')
+// arr.unshift ('monitor')
+// console.log (arr)
+
+// Question No. 19
+// var arr = ['apple','samsung','motorolla','nokia']
+// var option1 = document.querySelector ('#opt1')
+// var option2 = document.querySelector ('#opt2')
+// var option3 = document.querySelector ('#opt3')
+// var option4 = document.querySelector ('#opt4')
+
+// option1.innerHTML = arr[0]
+// option2.innerHTML = arr[1]
+// option3.innerHTML = arr[2]
+// option4.innerHTML = arr[3]
+
+// Question No. 20
+// var arr = [[223 , 333] , [44 , 444 , 55 , 5352 , 234 , 5 ,[4 , 34 , 3242 , 43 , 24]]]
+// console.log (arr)
+
+// Question No. 21
+// var arr = [[0 , 1 , 2 , 3],[1 , 0 , 1 , 2],[2 , 1 , 0 , 1]]
+// console.log (arr[0].join(' '))
+// console.log (arr[1].join(' '))
+// console.log (arr[2].join(' '))
+
+
